e8cd76e67acba09aebc03078c7ebd28ced39bd96
litmus-zhang <abdulsalamlukmon9@gmail.com>
2022-07-28 13:08:18 +0100

    rock-paper-scissors logic works fine

--- index.mjs
+++ index.mjs
@@ -9,11 +9,30 @@ const accBob = await stdlib.newTestAccount(startingBalance);
 const ctcAlice = accAlice.contract(backend)
 const ctcBob = accBob.contract(backend, ctcAlice.getInfo())
 
+const HAND = ['Rock', 'Paper', 'Scissors'];
+const OUTCOME = ['Bob Wins', 'Draw', 'Alice Wins'];
+
+const Player = (Who) =>(
+{
+        getHand: () =>
+        {
+        const hand = Math.floor(Math.random() * 3);
+        console.log(Who + ' played ' + HAND[hand]);
+        return hand;
+        },
+        seeOutcome: (outcome) =>
+        {
+            console.log(Who + ' saw outcome ' + OUTCOME[outcome]);
+        }
+})
+
 await Promise.all([
     ctcAlice.p.Alice({
+        ...Player('Alice')
 
     }),
     ctcBob.p.Bob({
+        ...Player('Bob')
 
     }),
 ])
--- index.rsh
+++ index.rsh
@@ -17,6 +17,20 @@ export const main = Reach.App(()=>{
 
     Alice.only(() =>
     {
-        
+        const handAlice = declassify(interact.getHand());
+    })
+    Alice.publish(handAlice);
+    commit();
+    Bob.only(() =>
+    {
+        const handBob = declassify(interact.getHand());
+    })
+    Bob.publish(handBob);
+
+    const outcome = (handAlice + (4 - handBob)) % 3;
+    commit()
+    each([Alice, Bob], () =>
+    {
+        interact.seeOutcome(outcome);
     })
 })
